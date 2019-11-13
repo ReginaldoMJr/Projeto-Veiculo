c6246f289f569c1c71d04a7ab4fb94caf5a3d635
ReginaldoMSJr <reginaldo.silva22@hotmail.com>
2019-11-13 12:05:18 -0300

    Ajustando Dirigir

--- Veiculo/Veiculo/Entities/CarroPercurso.cs
+++ Veiculo/Veiculo/Entities/CarroPercurso.cs
@@ -35,33 +35,44 @@ namespace Veiculo {
             }
             double viagem = MenuDirigir();
             CalculoClima(Veiculo, Percurso.Clima);
+            
             //Dirigir com todos os tipos de combustivel
             for (double km = 0; km <= Percurso.Trajeto; km = Math.Round((km + 0.1), 1)) {
+
                 if (Relatorio.KmPercorrida == Percurso.Trajeto) {
+                    Console.Clear();
                     agenciaViagem.CarroPercursos.Remove(agenciaViagem.CarroPercursos.Find(x => x.Percurso == Percurso));
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("Viagem finalizada, aperte enter para voltar ao menu");
                     Console.ResetColor();
                     Console.ReadLine();
                 }
+
                 else if (viagem == 0) {
+                    Console.Clear();
                     MenuViagem();
                     double MenuViagem() {
+                        Console.WriteLine("Chegou na sua parada\n\nO que deseja fazer?\n");
                         Console.WriteLine("[1] Abastecer\n\n[2] Calibrar Pneu\n\n[3] Continuar viagem");
                         string num = Console.ReadLine();
+
                         switch (num) {
+
                             case "1":
                                 if (Veiculo.Flex)
                                     Veiculo.AbastecerFlex();
                                 else
                                     Veiculo.Abastecer();
                                 return MenuViagem();
+
                             case "2":
                                 Veiculo.CalibrarPneu();
                                 return MenuViagem();
+
                             case "3":
                                 viagem = MenuDirigir();
                                 return viagem;
+
                             default:
                                 Console.WriteLine("Opcao invalida, aperte enter para tentar novamente");
                                 Console.ReadLine();
@@ -73,6 +84,10 @@ namespace Veiculo {
                     Veiculo.QtdGasolina = 0;
                     Veiculo.QtdAlcool = 0;
                     Veiculo.QtdCombustivel = 0;
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Combustivel acabou, Abasteça");
+                    Console.ResetColor();
                     Console.WriteLine($"Faltam {Percurso.Trajeto - Relatorio.KmPercorrida} KM");
                     Veiculo.EncherTanque();
                     Relatorio.QtdAbastecimentos++;
@@ -84,46 +99,56 @@ namespace Veiculo {
                     }
                     km = Math.Round((km - 0.1), 2);
                 }
+
                 else {
                     //Se for Flex
+
                     if (Veiculo.Flex) {
+
                         if (Veiculo.QtdAlcool > 0) {
-                            Veiculo.QtdAlcool = Math.Round((Veiculo.QtdAlcool - (0.1 / Veiculo.AutonomiaA)), 3);
-                            Relatorio.LitrosConsumidos = Math.Round((Relatorio.LitrosConsumidos + (0.1 / Veiculo.AutonomiaA)), 2);
+                            Veiculo.QtdAlcool = Math.Round((Veiculo.QtdAlcool - (0.1 / Veiculo.AutonomiaA)), 4);
+                            Relatorio.LitrosConsumidos = Math.Round((Relatorio.LitrosConsumidos + (0.1 / Veiculo.AutonomiaA)), 4);
                             Relatorio.KmPercorrida = Math.Round((Relatorio.KmPercorrida + 0.1), 1);
                             viagem = Math.Round((viagem - 0.1), 1);
                         }
+
                         else {
-                            Veiculo.QtdGasolina = Math.Round((Veiculo.QtdGasolina - (0.1 / Veiculo.AutonomiaG)), 3);
-                            Relatorio.LitrosConsumidos = Math.Round((Relatorio.LitrosConsumidos + (0.1 / Veiculo.AutonomiaG)), 2);
+                            Veiculo.QtdGasolina = Math.Round((Veiculo.QtdGasolina - (0.1 / Veiculo.AutonomiaG)), 4);
+                            Relatorio.LitrosConsumidos = Math.Round((Relatorio.LitrosConsumidos + (0.1 / Veiculo.AutonomiaG)), 4);
                             Relatorio.KmPercorrida = Math.Round((Relatorio.KmPercorrida + 0.1), 1);
                             viagem = Math.Round((viagem - 0.1), 1);
                         }
                     }
+
                     else {
+                        
                         //Se for Alcool
                         if (Veiculo.TipoCombustivel == "Alcool") {
-                            Veiculo.QtdCombustivel = Math.Round((Veiculo.QtdCombustivel - (0.1 / Veiculo.AutonomiaA)), 3);
-                            Relatorio.LitrosConsumidos = Math.Round((Relatorio.LitrosConsumidos + (0.1 / Veiculo.AutonomiaA)), 2);
+                            Veiculo.QtdCombustivel = Math.Round((Veiculo.QtdCombustivel - (0.1 / Veiculo.AutonomiaA)), 4);
+                            Relatorio.LitrosConsumidos = Math.Round((Relatorio.LitrosConsumidos + (0.1 / Veiculo.AutonomiaA)), 4);
                             Relatorio.KmPercorrida = Math.Round((Relatorio.KmPercorrida + 0.1), 1);
                             viagem = Math.Round((viagem - 0.1), 1);
                         }
+                        
                         //Se for Gasolina
                         else {
-                            Veiculo.QtdCombustivel = Math.Round((Veiculo.QtdCombustivel - (0.1 / Veiculo.AutonomiaG)), 3);
-                            Relatorio.LitrosConsumidos = Math.Round((Relatorio.LitrosConsumidos + (0.1 / Veiculo.AutonomiaG)), 2);
+                            Veiculo.QtdCombustivel = Math.Round((Veiculo.QtdCombustivel - (0.1 / Veiculo.AutonomiaG)), 4);
+                            Relatorio.LitrosConsumidos = Math.Round((Relatorio.LitrosConsumidos + (0.1 / Veiculo.AutonomiaG)), 4);
                             Relatorio.KmPercorrida = Math.Round((Relatorio.KmPercorrida + 0.1), 1);
                             viagem = Math.Round((viagem - 0.1), 1);
                         }
                     }
                 }
+
                 if (km % 100 == 0 && km > 0) {
                     int cli = new Random().Next(1, 4);
                     Relatorio.AlteracaoClimatica.AppendLine($"Alteracao Climatica: {km} KM -- Clima: {cli}");
                     Percurso.Clima = cli.ToString();
                     int pneu = new Random().Next(0, 2);
                     Veiculo.Pneu = (int.Parse(Veiculo.Pneu) - pneu).ToString();
+
                     if (Veiculo.Pneu == "0") {
+                        Console.Clear();
                         Console.WriteLine("Pneu Furou, calibrar");
                         Relatorio.DesgastePneu.AppendLine($"Desgaste de Pneu: {km} KM -- Pneu: {Veiculo.Pneu}");
                         Veiculo.Pneu = Veiculo.CalibrarPneu();
@@ -135,34 +160,42 @@ namespace Veiculo {
             }
         }
         public void CalculoClima(Veiculo veiculo, string Clima) {
+            
             //Clima Sol e Pneu Novo
             if (Clima == "1" && veiculo.Pneu == "3") {
                 veiculo.AutonomiaG = veiculo.AutonomiaOriginalG;
                 veiculo.AutonomiaA = veiculo.AutonomiaOriginalA;
             }
+            
             //Clima Sol e Pneu Seminovo
             else if (Clima == "1" && veiculo.Pneu == "2") {
                 veiculo.AutonomiaG = Math.Round((veiculo.AutonomiaOriginalG - (veiculo.AutonomiaOriginalG * 0.0725)), 2);
                 veiculo.AutonomiaA = Math.Round((veiculo.AutonomiaOriginalA - (veiculo.AutonomiaOriginalA * 0.0725)), 2);
             }
+            
             //Clima Sol e Pneu Velho
             else if (Clima == "1" && veiculo.Pneu == "1") {
                 veiculo.AutonomiaG = Math.Round((veiculo.AutonomiaOriginalG - (veiculo.AutonomiaOriginalG * 0.0915)), 2);
                 veiculo.AutonomiaA = Math.Round((veiculo.AutonomiaOriginalA - (veiculo.AutonomiaOriginalA * 0.0915)), 2);
             }
+            
             //Clima Chuva e Pneu Novo
             else if (Clima == "2" && veiculo.Pneu == "3") {
+
                 if (veiculo.Flex) {
                     veiculo.AutonomiaG = Math.Round((veiculo.AutonomiaOriginalG - (veiculo.AutonomiaOriginalG * 0.12)), 2);
                     veiculo.AutonomiaA = Math.Round((veiculo.AutonomiaG - (veiculo.AutonomiaG * 0.30)),2);
                 }
+
                 else if (veiculo.TipoCombustivel == "Gasolina")
                     veiculo.AutonomiaG = Math.Round((veiculo.AutonomiaOriginalG - (veiculo.AutonomiaOriginalG * 0.12)), 2);
+
                 else {
                     veiculo.AutonomiaA = Math.Round((veiculo.AutonomiaOriginalA - (veiculo.AutonomiaOriginalA * 0.12)), 2);
                     veiculo.AutonomiaA = Math.Round((veiculo.AutonomiaA - (veiculo.AutonomiaA * 0.30)), 2);
                 }
             }
+            
             //Clima Chuva e Pneu Seminovo
             else if (Clima == "2" && veiculo.Pneu == "2") {
                 if (veiculo.Flex) {
